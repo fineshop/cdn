874864543e64edbce0e2cfba4983084019f8719d
fineshop <inventordev480@gmail.com>
2022-12-11 15:10:54 +0530

    Updated assets

---
+++ assets/scripts/lazy.js
@@ -0,0 +1,23 @@
+//# lazy.js
+
+/* lazy function */
+const lazyCustomJs = function () {
+  ldJs("//cdn.jsdelivr.net/gh/fineshopdesign/cdn@assets/categoryPosts/categoryPosts.min.js","ctgry_js",!0,"body", function () {
+    getid('HTML2')!=null && (ctgryPst(ctgryLb1, '#HTML2 .ctgry', 6, 600, 200, true));
+    getid('HTML3')!=null && (ctgryPst(ctgryLb2, '#HTML3 .ctgry', 6, 600, 200, true));
+  });
+
+  /*if(typeof showAds!='undefined'&&isPost=="true"&&isPreview=="false"){var td=new Date();if((td.getDay()%2===0)&&PuSet.adsenseAds.publisherId!==""){(()=>{var adu=[2617042980,2684491596,7060116223,2921898342,3370860020];if(null==Pu.gC("G_CLICK")){var s=document.createElement("div");s.classList.add("gAd"),s.innerHTML="<div class='gCl hidden'><svg class='line' viewBox='0 0 24 24'><line x1='18' y1='6' x2='6' y2='18'></line><line x1='6' y1='6' x2='18' y2='18'></line></svg><div class='clDt'>"+Pu.de('G0uiK0lfb07frFjoCTNpdkB3bkBab043mZ45XLBcds68d0ovXk7fPNQ5dJBydqGfrFjoCTWorsYlb8Bodcp=')+"</div></div><div class='gCn'><ins class='adsbygoogle gAdC' style='display:block' data-ad-client='ca-pub-6450536625798530' data-ad-slot='"+Pu.rdm(adu)+"' data-ad-format='auto' data-full-width-responsive='false'></ins></div>",qSel("body").appendChild(s),(gAdC=window.adsbygoogle||[]).push({});const l=s.querySelector("ins.gAdC");var a=()=>{Pu.sC("G_CLICK",1,{secure:1,"max-age":43200}),toastNotif(Pu.de('YsYkmZX4mZ4qCJoyrUCfK0uiK0lvOc7=')),setTimeout(()=>{s.remove(),toastNotif(Pu.de('G0uiK0lfYsYkmZXiXZGvOc7='))},3e3)};l.addEventListener("click",()=>{a()});let t=setInterval(()=>{var e=s.querySelector("ins.gAdC iframe");"unfilled"===l.getAttribute("data-ad-status")&&(clearInterval(t),s.remove()),null!==e&&"filled"===l.getAttribute("data-ad-status")&&(clearInterval(t),s.querySelector(".gCl").classList.remove("hidden"),window.addEventListener("blur",()=>{document.activeElement==e&&a()}))},100)}})();}}*/
+  /*if(isPost=="false"&&isPreview=="false"){(()=>{let e=document.getElementsByTagName("body")[0],t=document.getElementsByTagName("script")[0],i=document.createElement("div");i.id="fb-chatElem",i.innerHTML="<div id='fb-root'></div><div id='fb-customer-chat' class='fb-customerchat' page_id='109863708244210' attribution='biz_inbox'></div>",window.fbAsyncInit=()=>{FB.init({xfbml:!0,version:"v14.0"})};let n=document.createElement("script");n.id="fb-sdk",n.src="https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js",e.appendChild(i),t.parentNode.insertBefore(n,t)})()}*/
+  // Clarity
+  (function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+  })(window, document, "clarity", "script", "e5txsdmymw");
+}
+
+/* Scroll function */
+const scrollCustomJs = function () {
+  
+}
